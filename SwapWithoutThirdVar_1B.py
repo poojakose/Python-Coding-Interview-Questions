85cc4722ae184fd7387d5b9bfb5d104a38d9d169
Pooja Kose <30698792+poojakose@users.noreply.github.com>
2024-01-21 23:09:54 +0530

    Create SwapWithoutThirdVar_1B.py
    Python interview questions - Coding
    Swap or exchange values of two variables without using any third or temporary variable using Multiplication and division operator method in Python .
    
    
    #python #pythoninterview #swap #exchange #pythonprogramming #pythontutorial #python3 #interview

---
+++ SwapWithoutThirdVar_1B.py
@@ -0,0 +1,12 @@
+x = 10
+y = 30
+
+print("Before Swap :", x, y)
+
+x = x * y # 10 * 30 = 300
+y = x / y # 300 / 30 = 10
+x = x / y # 300 / 10 = 30
+
+#print("After Swap :", x, y)
+
+print("After Swap int :", int(x), int(y))
